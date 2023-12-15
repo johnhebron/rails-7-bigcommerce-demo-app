144048d9dca61589cde1d69ebbb5d36cf4ac9f36
John Hebron <johnhebron@gmail.com>
2023-12-14 19:17:48 -0600

    remove x-frame-options header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  after_action :set_header_for_iframe
+
+  private
+
+  def set_header_for_iframe
+    response.headers.delete "X-Frame-Options"
+  end
 end
